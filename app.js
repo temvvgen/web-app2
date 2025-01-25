ab9cb40001c84170c8b97417d0331c5c40b98b1b
temvvgen <temvvgenbaljin0129@gmail.com>
2025-01-25 16:49:39 +0800

    ustgah vildliiig delgetsend hiile

--- app.js
+++ app.js
@@ -40,16 +40,23 @@ var uiController = (function () {
       }
     },
 
+    deleteListItem: function (id) {
+      var el = document.getElementById(id);
+      if (el) {
+        el.parentNode.removeChild(el);
+      }
+    },
+
     addListItem: function (item, type) {
       var html, list;
       if (type === "inc") {
         list = ".income__list";
         html =
-          '<div class="item clearfix" id="income-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="inc-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else {
         list = ".expenses__list";
         html =
-          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">%VALUE%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>';
       }
 
       html = html.replace("%id%", item.id);
@@ -114,6 +121,18 @@ var financeController = (function () {
         totalExp: data.totals.exp,
       };
     },
+
+    deleteItem: function (type, id) {
+      var ids = data.items[type].map(function (el) {
+        return el.id;
+      });
+
+      var index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.items[type].splice(index, 1);
+      }
+    },
     addItem: function (type, desc, val) {
       var item, id;
 
@@ -166,6 +185,22 @@ var appController = (function (uiController, financeController) {
         addController();
       }
     });
+
+    document
+      .querySelector(".container")
+      .addEventListener("click", function (event) {
+        var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        if (id) {
+          var arr = id.split("-");
+          var type = arr[0];
+          var itemId = parseInt(arr[1]);
+
+          console.log(type + " - " + itemId);
+          financeController.deleteItem(type, itemId);
+
+          uiController.deleteListItem(id);
+        }
+      });
   };
 
   return {
